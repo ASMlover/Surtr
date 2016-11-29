1bfc60d50c225f3c69bb42489966859b25cbd799
ASMlover <asmlover@126.com>
2016-11-29 23:42:59 +0800

    :sparkles: feat(surtr.script): add Lexer for surtr script

--- surtr/script/Lexer.py
+++ surtr/script/Lexer.py
@@ -69,8 +69,102 @@ class Token(object):
         type_text = TokenNames.get(self.type, '<INVALID>')
         return '>>>>>>>>>> {value: "%s", type: %s, line: %d}' % (self.value, type_text, self.line)
 
+class LexerException(Exception):
+    def __init__(self, message, lineno):
+        self.message = message
+        self.lineno = lineno
+
+    def __str__(self):
+        return 'Error at line(%d) - %s' % (self.lineno, self.message)
+
 class Lexer(object):
-    pass
+    def __init__(self, content=''):
+        self.content = content
+        self.pos = 0
+        self.lineno = 1
+
+    def get_char(self):
+        if self.pos >= len(self.content):
+            return None
+
+        c = self.content[self.pos]
+        self.pos += 1
+        return c
+
+    def unget_char(self):
+        assert self.pos > 0, 'Lexer pos can not be neigative'
+        self.pos -= 1
+
+    def skip_line_comment(self):
+        while True:
+            c = self.get_char()
+            if c is None or c == '\r' or c == '\n':
+                if c == '\r' or c == '\n':
+                    if c == '\r' and self.get_char() != '\n':
+                        self.unget_char()
+                    self.lineno += 1
+                break
+
+    def get_string(self):
+        char_list = []
+        while True:
+            c = self.get_char()
+            if c is None or c == '\r' or c == '\n':
+                if c == '\r' and self.get_char() != '\n':
+                    self.unget_char()
+                raise LexerException('need close const string symbol `"`', self.lineno)
+            elif c == '"':
+                return Token(''.join(char_list), TokenType.STRING, self.lineno)
+            else:
+                char_list.append(c)
+
+    def get_identifier(self):
+        self.unget_char()
+
+        char_list = []
+        while True:
+            c = self.get_char()
+            if str.isalnum(c) or c == '_':
+                char_list.append(c)
+            else:
+                self.unget_char()
+                return Token(''.join(char_list), TokenType.IDENTIFIER, self.lineno)
+
+    def get_token(self):
+        while True:
+            c = self.get_char()
+            if c is None:
+                return Token(None, TokenType.EOF, self.lineno)
+            elif c in (' ', '\t',):
+                continue
+            elif c == '\r':
+                if self.get_char() != '\n':
+                    self.unget_char()
+                self.lineno += 1
+            elif c == '\n':
+                self.lineno += 1
+            elif c == '#':
+                self.skip_line_comment()
+            elif c == '"':
+                return self.get_string()
+            elif str.isalpha(c) or c == '_':
+                return self.get_identifier()
+            elif c == ',':
+                return Token(',', TokenType.COMMA, self.lineno)
+            elif c == '(':
+                return Token('(', TokenType.LPAREN, self.lineno)
+            elif c == ')':
+                return Token(')', TokenType.RPAREN, self.lineno)
+            else:
+                return Token(None, TokenType.ERROR, self.lineno)
 
 if __name__ == '__main__':
-    print (Token('print', TokenType.IDENTIFIER, 0))
+    with Utils.do_open("demo.su", mode='r', encoding='utf-8') as fp:
+        content = fp.read()
+        lex = Lexer(content)
+
+        while True:
+            tok = lex.get_token()
+            if tok.type in (TokenType.ERROR, TokenType.EOF):
+                break
+            print (tok)
